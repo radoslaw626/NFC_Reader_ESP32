ae9a0c22e9d71cf3fed37d864726be59938bd2bc
Karol Dubas <krizz.kontakt@gmail.com>
2021-07-21 12:31:01 +0200

    Poprawono zapis pliku, dodano wyswietlanie zapisu LED

--- write_uifd_to_sd_csv/write_uifd_to_sd_csv.ino
+++ write_uifd_to_sd_csv/write_uifd_to_sd_csv.ino
@@ -6,19 +6,30 @@
 #include <Wire.h>
 #include <string>
 
-#define DS1302_CLK_PIN      14
-#define DS1302_IO_PIN       12
-#define DS1302_CE_PIN       13
+// DS1302
+const uint8_t DS1302_CLK_PIN = 14;
+const uint8_t DS1302_IO_PIN = 12;
+const uint8_t DS1302_CE_PIN = 13;
 
-constexpr uint8_t MFRC522_RST_PIN = 4;
-constexpr uint8_t MFRC522_SS_PIN =  5;
-constexpr uint8_t SD_CS_PIN =  15;
+// MFRC522
+const uint8_t MFRC522_RST_PIN = 4;
+const uint8_t MFRC522_SS_PIN = 5;
+
+// SD
+const uint8_t SD_CS_PIN = 15;
+
+// LED
+const uint8_t LED_RED = 25;
+const uint8_t LED_GREEN = 26;
+const uint8_t LED_BLUE = 27;
 
 ErriezDS1302 rtc = ErriezDS1302(DS1302_CLK_PIN, DS1302_IO_PIN, DS1302_CE_PIN);
 MFRC522 rfid(MFRC522_SS_PIN, MFRC522_RST_PIN); // Instance of the class
 MFRC522::MIFARE_Key key;
+// String NUID;
 char NUID[25];
 const String FILE_NAME = "db.csv";
+File myFile;
 
 void setup() {
   // Open serial communications and wait for port to open:
@@ -34,15 +45,23 @@ void setup() {
     key.keyByte[i] = 0xFF;
   }
 
+  // Diodes init
+  pinMode(LED_RED, OUTPUT);
+  pinMode(LED_GREEN, OUTPUT);
+  pinMode(LED_BLUE, OUTPUT);
+  digitalWrite(LED_RED, HIGH);
+  digitalWrite(LED_GREEN, HIGH);
+  digitalWrite(LED_BLUE, HIGH);
+
   Serial.println(F("This code scan the MIFARE Classsic NUID."));
-  Serial.print(F("Using the following key:"));
+  Serial.print(F("Using the following key: "));
   printHex(key.keyByte, MFRC522::MF_KEY_SIZE);
 
   // SD card init
   Serial.print("\r\nInitializing SD card...");
   SD.begin(SD_CS_PIN);
   if (!SD.begin(SD_CS_PIN)) {
-    Serial.println("Initialization failed!");
+    Serial.println("\r\nInitialization failed!");
     while (1);
   }
   Serial.println("\r\nInitialization done.");
@@ -122,7 +141,9 @@ int readNUID()
   // Verify if the NUID has been readed
   if ( ! rfid.PICC_ReadCardSerial())
     return 0;
-
+    
+  digitalWrite(LED_BLUE, LOW);
+  
   Serial.print(F("PICC type: "));
   MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
   Serial.println(rfid.PICC_GetTypeName(piccType));
@@ -156,11 +177,12 @@ int readNUID()
   return 1;
 }
 
+// Po wpieciu karty pierwszy odczyt ma puste NUID.
 void saveNUID()
 {
   // open the file, note that only one file can be open at a time,
   // so you have to close this one before opening another.
-  File myFile = SD.open("/" + FILE_NAME, FILE_APPEND);
+  myFile = SD.open("/" + FILE_NAME, FILE_APPEND);
 
   // if the file opened okay, write to it:
   if (myFile) {
@@ -171,10 +193,18 @@ void saveNUID()
 
     // close the file:
     myFile.close();
+    digitalWrite(LED_BLUE, HIGH);
+    digitalWrite(LED_GREEN, LOW);
+    delay(1000);
+    digitalWrite(LED_GREEN, HIGH); 
     Serial.println("\r\nSaved, file closed.");
   } else {
     // if the file didn't open, print an error:
     Serial.println("Error opening " + FILE_NAME);
+    digitalWrite(LED_BLUE, HIGH);
+    digitalWrite(LED_RED, LOW); 
+    delay(1000);
+    digitalWrite(LED_RED, HIGH);
   }
 
   // re-open the file for reading:
@@ -190,7 +220,7 @@ void saveNUID()
     myFile.close();
   } else {
     // if the file didn't open, print an error:
-    Serial.println("error opening " + FILE_NAME);
+    Serial.println("Error opening " + FILE_NAME);
   }
 }
 
